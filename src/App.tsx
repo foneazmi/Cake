8f93da4f659e3e6499f7092581e7287f44e8cad3
Farkhan Azmi <ghost@Farkhans-MacBook-Pro.local>
2022-10-02 22:22:18 +0700

    fix test script

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
-import {AppRouter} from './AppRouter';
+import {AppRouter} from './router';
 
 export default function App() {
   return (

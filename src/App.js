380d70916513753f666e2a129339b067434dea46
Farkhan Azmi <ghost@192.168.1.7>
2022-12-13 19:07:41 +0700

    some update on codepush

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AppRouter} from './router';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import {store, persistor} from './stores';
 import codePush from 'react-native-code-push';
 
-let codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-};
-
 const App = () => {
+  useEffect(() => {
+    const codePushStatusDidChange = status => {
+      switch (status) {
+        case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+          console.log('Checking for updates.');
+          break;
+        case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+          console.log('Downloading package.');
+          break;
+        case codePush.SyncStatus.INSTALLING_UPDATE:
+          console.log('Installing update.');
+          break;
+        case codePush.SyncStatus.UP_TO_DATE:
+          console.log('Up-to-date.');
+          break;
+        case codePush.SyncStatus.UPDATE_INSTALLED:
+          console.log('Update installed.');
+          break;
+      }
+    };
+
+    const codePushDownloadDidProgress = progress => {
+      console.log(
+        progress.receivedBytes + ' of ' + progress.totalBytes + ' received.',
+      );
+    };
+    const syncImmediate = () => {
+      console.log('synced');
+      codePush.sync(
+        {
+          checkFrequency: codePush.CheckFrequency.ON_APP_START,
+          installMode: codePush.InstallMode.IMMEDIATE,
+          updateDialog: {
+            appendReleaseDescription: true,
+            optionalUpdateMessage: 'Updates here..',
+            title: 'New Updates',
+            optionalInstallButtonLabel: 'Yes',
+            optionalIgnoreButtonLabel: 'No',
+          },
+        },
+        codePushStatusDidChange,
+        codePushDownloadDidProgress,
+      );
+    };
+    syncImmediate();
+  });
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
@@ -19,4 +62,4 @@ const App = () => {
   );
 };
 
-export default codePush(codePushOptions)(App);
+export default codePush(App);
